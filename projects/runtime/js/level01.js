b2b5a0fdefdf82b499bcf6904cb41943ccff5879
blakecaire <blakejcaire24@gmail.com>
2023-04-17 18:19:26 +0000

    runtime

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -5,7 +5,7 @@ var level01 = function (window) {
     var draw = window.opspark.draw;
     var createjs = window.createjs;
 
-    window.opspark.runLevelInGame = function(game) {
+    window.opspark.runLevelInGame = function (game) {
         // some useful constants 
         var groundY = game.groundY;
 
@@ -13,23 +13,23 @@ var level01 = function (window) {
         // behavior of our game
         var levelData = {
             "name": "Robot Romp",
-            "number": 1, 
+            "number": 1,
             "speed": -3,
             "gameItems": [
-                
+
                 { "type": "sawblade", "x": 400, "y": groundY - 110 },
-                { "type": "sawblade", "x": 600, "y": groundY - 10},
-                { "type": "sawblade", "x": 900, "y": groundY - 110},
+                { "type": "sawblade", "x": 600, "y": groundY - 10 },
+                { "type": "sawblade", "x": 900, "y": groundY - 110 },
 
-                { "type": "enemy", "x": 400, "y": groundY - 50},
-                { "type": "enemy", "x": 600, "y": groundY-60},
+                { "type": "enemy", "x": 400, "y": groundY - 50 },
+                { "type": "enemy", "x": 600, "y": groundY - 60 },
 
-                { "type": "reward", "x": 600, "y": groundY-100},
+                { "type": "reward", "x": 600, "y": groundY - 100 },
             ]
         };
         window.levelData = levelData;
         // set this to true or false depending on if you want to see hitzones
-        game.setDebugMode(true);
+        game.setDebugMode(false);
 
         // TODO 6 and on go here
         // BEGIN EDITING YOUR CODE HERE
@@ -44,8 +44,8 @@ var level01 = function (window) {
         sawBladeHitZone.addChild(obstacleImage); // makes obstacleImage as a child of sawBladeHitZone
         obstacleImage.x = -25; //adjusts the images x to align with the hit zone
         obstacleImage.y = -25; //adjusts the images y to align with the hit zone
-        */ 
-        function createSawBlade(x,y){
+        */
+        function createSawBlade(x, y) {
             var hitZoneSize = 25; //assigns a value of 25 as the hitzone size
             var damageFromObstacle = 10; //assigns a value of 10 as the damage for hitting the obstacle
             var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle); //creates the obstacle and stores it in the variable sawBladeHitZone
@@ -57,7 +57,7 @@ var level01 = function (window) {
             obstacleImage.x = -25; //adjusts the images x to align with the hit zone
             obstacleImage.y = -25; //adjusts the images y to align with the hit zone
         }
-        function createSpike(x, y){
+        function createSpike(x, y) {
             var hitZoneSize = 25; //assigns a value of 25 as the hitzone size
             var damageFromObstacle = 10; //assigns a value of 10 as the damage for hitting the obstacle
             var spikeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle); //creates the obstacle and stores it in the variable spikeHitZone
@@ -69,61 +69,61 @@ var level01 = function (window) {
             obstacleImage.x = -30; //adjusts the images x to align with the hit zone
             obstacleImage.y = -30; //adjusts the images y to align with the hit zone
         }
-        function createEnemy(x, y){
-            var enemy = game.createGameItem("enemy", 25);
-            var gameItem = draw.rect(50, 50, "red");
-            gameItem.x = -25;
-            gameItem.y = -25;
-            enemy.addChild(gameItem);
+        function createEnemy(x, y) {
+            var enemy = game.createGameItem("enemy", 25); // creates a enemy and stores it to variable enemy
+            var redSquare = draw.rect(50, 50, "red"); 
+            redSquare.x = -25;
+            redSquare.y = -25;
+            enemy.addChild(redSquare);
             enemy.x = x;
             enemy.y = y;
             game.addGameItem(enemy);
             enemy.velocityX = -3;
-            
 
-            enemy.onPlayerCollision = function (){
+
+            enemy.onPlayerCollision = function () {
                 game.changeIntegrity(-10);
             }
-            enemy.onProjectileCollision = function (){
+            enemy.onProjectileCollision = function () {
                 game.increaseScore(100);
                 enemy.fadeOut();
             }
         }
-        function createReward(x, y){
+        function createReward(x, y) {
             var reward = game.createGameItem("reward", 25);
-            var gameItem = draw.rect(50, 50, "blue");
-            gameItem.x = -25;
-            gameItem.y = -25;
-            reward.addChild(gameItem);
+            var blueSquare = draw.rect(50, 50, "blue");
+            blueSquare.x = -25;
+            blueSquare.y = -25;
+            reward.addChild(blueSquare);
             reward.x = x;
             reward.y = y;
             game.addGameItem(reward);
             reward.velocityX = -3;
-            
 
-            reward.onPlayerCollision = function (){
+
+            reward.onPlayerCollision = function () {
                 game.increaseScore(100);
                 game.changeIntegrity(10);
                 reward.fadeOut();
             }
-            reward.onProjectileCollision = function (){
+            reward.onProjectileCollision = function () {
                 reward.fadeOut();
             }
         }
-        for( var i = 0; i < levelData.gameItems.length; i++){
+        for (var i = 0; i < levelData.gameItems.length; i++) {
             var gameItem = levelData.gameItems[i];
-            if(gameItem.type === "sawblade"){
+            if (gameItem.type === "sawblade") {
                 createSawBlade(gameItem.x, gameItem.y);
-            } 
-            if(gameItem.type === "enemy"){
+            }
+            if (gameItem.type === "enemy") {
                 createEnemy(gameItem.x, gameItem.y);
-            } 
-            if(gameItem.type === "reward"){
+            }
+            if (gameItem.type === "reward") {
                 createReward(gameItem.x, gameItem.y);
             }
-            if(gameItem.type === "spike"){
+            if (gameItem.type === "spike") {
                 createSpike(gameItem.x, gameItem.y);
-            } 
+            }
         }
 
         // DO NOT EDIT CODE BELOW HERE
@@ -131,7 +131,7 @@ var level01 = function (window) {
 };
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
-if((typeof process !== 'undefined') &&
+if ((typeof process !== 'undefined') &&
     (typeof process.versions.node !== 'undefined')) {
     // here, export any references you need for tests //
     module.exports = level01;
